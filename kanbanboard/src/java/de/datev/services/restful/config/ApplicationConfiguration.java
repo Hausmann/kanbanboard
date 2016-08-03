bf79f987b3096ee699e841f7958f90a62c670867
TG00028 <TG00028@192.168.1.107>
2016-08-03 14:53:59 +0200

    ddddsf

---
+++ kanbanboard/src/java/de/datev/services/restful/config/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package de.datev.services.restful.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/kanbanboard/")
+public class ApplicationConfiguration extends Application {
+
+}
