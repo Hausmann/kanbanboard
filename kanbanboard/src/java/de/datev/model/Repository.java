287113a1fbc6e88e099827f56b38d3cc376f9e28
TG00029 <TG00029@192.168.1.108>
2016-08-04 16:27:40 +0200

    Bootstrap fonts added

--- kanbanboard/src/java/de/datev/model/Repository.java
+++ kanbanboard/src/java/de/datev/model/Repository.java
@@ -1,23 +1,44 @@
 package de.datev.model;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.json.Json;
+import javax.json.JsonWriter;
 
 public class Repository {
     
-    
+    private final static String path = "./data.json";
     
     private static ArrayList<Task> tasks = new ArrayList<Task>();
     private static ArrayList<TaskList> taskLists = new ArrayList<TaskList>();
 
     public static ArrayList<Task> getTasks() {
         
-        
+        saveChanges();
         
         return tasks;
     }
 
     public static ArrayList<TaskList> getTaskLists() {
+        saveChanges();
+        
         return taskLists;
+    }
+
+    private static void saveChanges() {
+        FileOutputStream out;
+        try {
+            out = new FileOutputStream(path);
+            JsonWriter writer = Json.createWriter(out); 
+            writer.write(tasks);
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
     
