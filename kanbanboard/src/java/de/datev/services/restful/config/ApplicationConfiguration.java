4f4c53f4c6e3d57b8af89f5f9bcd11ab7abbaa7b
TG00026 <TG00026@192.168.1.105>
2016-08-04 13:12:26 +0200

    Merge origin/team1 into team1
    Conflicts:
    	kanbanboard/web/index.html

--- kanbanboard/src/java/de/datev/services/restful/config/ApplicationConfiguration.java
+++ kanbanboard/src/java/de/datev/services/restful/config/ApplicationConfiguration.java
@@ -1,9 +1,11 @@
 package de.datev.services.restful.config;
 
+import de.datev.model.KanbanListModel;
+import java.util.ArrayList;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/kanbanboard/")
 public class ApplicationConfiguration extends Application {
-
+    
 }
