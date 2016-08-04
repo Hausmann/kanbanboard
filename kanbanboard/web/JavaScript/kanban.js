ed3ed3b4442f97fb4a3e37025af18d6a99a89688
TG00025 <TG00025@192.168.1.104>
2016-08-04 14:35:27 +0200

    merged

--- kanbanboard/web/JavaScript/kanban.js
+++ kanbanboard/web/JavaScript/kanban.js
@@ -34,26 +34,24 @@ function AddItemDialog()
     var btn = document.getElementById("addItem");
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    var span = document.getElementById("closeItem");
 
-// When the user clicks on the button, open the modal
+    // When the user clicks on the button, open the modal
     btn.onclick = function () {
         modal.style.display = "block";
     }
 
-// When the user clicks on <span> (x), close the modal
+    // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
     }
 
-// When the user clicks anywhere outside of the modal, close it
+    // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
     }
-    // Ausgabe von Hallo Name mit Prompt- und Alert-Box
-    //var sName = prompt("Bitte Beschreibung eingeben:");
 }
 
 function AddListDialog(){
@@ -64,7 +62,7 @@ function AddListDialog(){
     var btn = document.getElementById("addList");
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    var span = document.getElementById("closeList");
 
     // When the user clicks on the button, open the modal
     btn.onclick = function () {
